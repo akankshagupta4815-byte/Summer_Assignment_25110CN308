959bde2b90ab7e73d586e0d8581137899fa7cb35
Akanksha gupta <25110cn308@glbitm.ac.in>
2026-06-11 21:55:18 +0530

    Added day 11

---
+++ day 11/program43.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int Prime(int n){
+    int i;
+
+    if(n <= 1){
+        return 0;
+    }
+    for(i = 2; i < n; i++){
+        if(n % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+int main (){
+    int num;
+    printf("enter a number:");
+    scanf("%d",&num);
+    if(Prime(num)){
+        printf("Prime number");
+    }
+    else{
+        printf("Not a Prime number");
+    }
+    return 0;
+}
